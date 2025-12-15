0abd1983dc4da7b8cfc69b684bf0e53b494e4914
steeve pires madeira <119943572+spiresm@users.noreply.github.com>
2025-12-15 12:02:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-```typescript
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 );
-```
+
