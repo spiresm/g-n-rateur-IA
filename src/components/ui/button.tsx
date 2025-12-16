7ebb33de03f217ed544cd0d64e6d4fa7e3dfc5b9
steeve pires madeira <119943572+spiresm@users.noreply.github.com>
2025-12-16 12:47:51 +0100

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -28,3 +28,19 @@ const buttonVariants = cva(
     },
   },
 );
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "button";
+  return <Comp className={cn(buttonVariants({ variant, size, className }))} {...props} />;
+}
+
+export { Button, buttonVariants };
