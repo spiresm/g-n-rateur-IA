dc2c7ec2f465e36e73ba1d3862508d58334a1a5c
steeve pires madeira <119943572+spiresm@users.noreply.github.com>
2025-12-19 17:51:48 +0100

    Create PosterForm.tsx

---
+++ src/components/poster/PosterForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface PosterFormProps {
+  poster: any;
+  isGenerating: boolean;
+}
+
+export function PosterForm({ poster, isGenerating }: PosterFormProps) {
+  return (
+    <>
+      {/* 🔽 COLLE ICI LE JSX DU FORMULAIRE
+          (tout ce qui était dans PosterGenerator,
+           SAUF les hooks et les handlers) */}
+    </>
+  );
+}
